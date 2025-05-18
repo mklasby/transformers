5a24e041d84439a4650f6a42d83a453e2110d9b5
Mike Lasby <mklasby@gmail.com>
2025-05-18 06:20:09 -0600

    minimal specbench patch applied to generation/utils.py to add AR statistics

--- src/transformers/generation/utils.py
+++ src/transformers/generation/utils.py
@@ -4677,8 +4677,12 @@ class GenerationMixin:
         model_kwargs = self._get_initial_cache_position(input_ids, model_kwargs)
 
         this_peer_finished = False
+        # TODO: Add config for returning specdecode statistics
+        step = 0
+        accept_length_list = []
         is_first_iteration = True  # to preserve the same API in the output as other generation methods
         while self._has_unfinished_sequences(this_peer_finished, synced_gpus, device=input_ids.device):
+            step += 1
             cur_len = input_ids.shape[-1]
 
             #  1. Fetch candidate sequences from a `CandidateGenerator` and move to the correct device
@@ -4774,6 +4778,8 @@ class GenerationMixin:
             # 4.2. Discard past key values relative to unused assistant tokens
             new_cache_size = new_cur_len - 1
             outputs.past_key_values = _crop_past_key_values(self, outputs.past_key_values, new_cache_size)
+            accept_length_tree = new_cur_len - cur_len
+            accept_length_list.append(accept_length_tree)
 
             # 5. Update the candidate generation strategy if needed
             candidate_generator.update_candidate_strategy(input_ids, new_logits, n_matches)
@@ -4843,6 +4849,7 @@ class GenerationMixin:
             candidate_generator.assistant_model.generation_config.num_assistant_tokens = (
                 candidate_generator.num_assistant_tokens
             )
+        idx = step - 1
         if return_dict_in_generate:
             if self.config.is_encoder_decoder:
                 return GenerateEncoderDecoderOutput(
@@ -4866,7 +4873,7 @@ class GenerationMixin:
                     past_key_values=model_kwargs.get("past_key_values"),
                 )
         else:
-            return input_ids
+            return input_ids, idx, accept_length_list
 
     def _prefill_chunking(self, input_ids: torch.LongTensor, generation_config: GenerationConfig, **model_kwargs):
         # Even if we are not compiling the forward, flex is always compiled when used. With chunk prefill, we may
